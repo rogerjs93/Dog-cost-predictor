004a00e579a6e5e294ac8eb315e3dfffab46c951
Roger Salas <58913883+rogerjs93@users.noreply.github.com>
2023-03-07 11:19:58 +0200

    Add files via upload

---
+++ tableConvert.com_jouz6t.sql
@@ -0,0 +1,33 @@
+﻿CREATE TABLE tableName 
+(
+    Age	VARCHAR(512),
+    Gender	VARCHAR(512),
+    Coat Color	VARCHAR(512),
+    Pedigree	VARCHAR(512),
+    Location	VARCHAR(512),
+    Breeder reputation	VARCHAR(512),
+    Size	VARCHAR(512),
+    Health	VARCHAR(512),
+    Trainning	VARCHAR(512),
+    	VARCHAR(512)
+);
+
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('3 months', 'Male', '1. Orange', 'Champion', 'America', 'Excellent', 'Teacup', 'Chronic Health issues', 'Basic trainning', '');
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('6 months', 'Female', '2. Red', 'Grand Champion ', 'Europe', 'Good', 'Toy', 'Genetic issues', 'Advance trainning', '');
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('1 year', '', '3. Cream', 'Multiple champion', 'Asia', 'Poor', 'Mini', '', 'Theraphy trainning', '');
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('2 year', '', '4. Sable', 'Champion lines pedigree', '', '', '', '', '', '');
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('3 year', '', '5. Black', 'Show quality pedigree', '', '', '', '', '', '');
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('4 year', '', '6. Blue', 'Pet quality pedigree', '', '', '', '', '', '');
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('5 year', '', '7. Chocolate', '', '', '', '', '', '', '');
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('6 year', '', '8. Merle', '', '', '', '', '', '', '');
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('7 year', '', '9. Brindle', '', '', '', '', '', '', '');
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('8 year', '', '10. Parti ', '', '', '', '', '', '', '');
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('9 year', '', '11. Beaver ', '', '', '', '', '', '', '');
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('10 year', '', '12. Wolf Sable ', '', '', '', '', '', '', '');
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('11 year', '', '', '', '', '', '', '', '', '');
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('12 year', '', '', '', '', '', '', '', '', '');
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('13 year', '', '', '', '', '', '', '', '', '');
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('14 year', '', '', '', '', '', '', '', '', '');
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('15 year', '', '', '', '', '', '', '', '', '');
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('16 year', '', '', '', '', '', '', '', '', '');
+INSERT INTO tableName (Age, Gender, Coat Color, Pedigree, Location, Breeder reputation, Size, Health, Trainning, ) VALUES ('17 year', '', '', '', '', '', '', '', '', '');
